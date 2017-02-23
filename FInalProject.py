6a2b0e87fee3a5fec11e3b1f03ae8cff7fd2a1a3
Jason Fraizer <jasonf56@cs.washington.edu>
2017-02-22 16:27:18 -0800

    Updated FinalProject.py

--- FInalProject.py
+++ FInalProject.py
@@ -1,27 +1,53 @@
 import numpy as np
 import pandas as pd
+import random
+import math
+
+SIZE_TRAINING = 0.8
 
 def processData():
     detailed_results = pd.read_csv('RegularSeasonDetailedResults.csv')
-    df_1 = pd.DataFrame()
-    df_1[['team1', 'team2']] = detailed_results[['Wteam', 'Lteam']].copy()
-    df_1['pred'] = 1
-    df_2 = pd.DataFrame()
-    df_2[['team1', 'team2']] = detailed_results[['Lteam', 'Wteam']].copy()
-    df_2['pred'] = 0
-    df = pd.concat((df_1, df_2), axis=0)
+    data = detailed_results.values
+    np.random.shuffle(data)
+    row = int(math.floor(0.8 * data.shape[0]))
+    training = data[0:row - 1, :]
+    test = data[row:data.shape[0] - 1, :]
 
-    train = df.values
-    np.random.shuffle(train)
+    return training, test
+
+def logisticRegressionSGD(dataset, weights, stepSize):
+
+    order = []
+    for i in range(1, dataset.shape[0]):
+        order.append(i)
+
+    random.shuffle(order)
+
+    for k in range(0, 2):
+        for i in order:
+            for j in range(1, dataset.shape[1]):
+                weightTranspose = np.transpose(weights)
+                dotProduct = np.dot(weightTranspose, dataset[i])
+                lastTerm = sigmoid(dotProduct)
+                partial = dataset[i, j] * (dataset[i, 0] - lastTerm)
+                weights[j] -= partial * stepSize
 
 
-    print(df)
+    return weights
+
+def sigmoid(x):
+    try:
+        return 1 / (1 + math.exp(-x))
+    except OverflowError:
+        return 1
 
 
 
 def main():
-    processData()
+    training, test = processData()
 
+    print(training)
+    print(test)
 
 if __name__ == "__main__":
     main()
