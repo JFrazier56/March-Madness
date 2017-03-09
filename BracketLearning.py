7437447f389dc32d9a180c03eb85713da31f5d8c
studentuser <studentuser@D-69-91-143-223.dhcp4.washington.edu>
2017-03-09 11:06:59 -0800

    Started bracketTraining.py

--- BracketLearning.py
+++ BracketLearning.py
@@ -24,7 +24,12 @@ def processData():
     training = data[0:row - 1, :]
     test = data[row:data.shape[0] - 1, :]
 
-    return training, test
+    bracket_results = pd.read_csv("Data/.....")
+    bracket_data = bracket_results.data
+
+    np.random.shuffle(bracket_data)
+
+    return training, test, bracket_data
 
 
 # Run Logistic regression with L2 normalization on the dataset
@@ -43,8 +48,8 @@ def logisticRegressionSGD(dataset, results, weights, stepSize, lambdaValue, iter
     return weights
 
 # Compute the perecentage of corrent guesses we get with the newly learned weights from SGD
-def testingNewWeight(dataset, results, weights):
-    LogLoss = 0
+def logisticRegressionPredict(dataset, weights):
+    predictedValues = []
     for i in range(0, dataset.shape[0]):
         weightTranspose = np.transpose(weights)
         dotProduct = np.dot(weightTranspose, dataset[i])
@@ -54,10 +59,9 @@ def testingNewWeight(dataset, results, weights):
         else:
             yValue = 0.0
 
-        if results[i] == yValue:
-            LogLoss += 1.0
+        predictedValues += [yValue]
 
-    return LogLoss / dataset.shape[0]
+    return predictedValues
 
 # Function to compute the sigmoid
 def sigmoid(x):
@@ -73,74 +77,53 @@ def shuffle_in_unison(a, b):
     np.random.set_state(rng_state)
     np.random.shuffle(b)
 
-# Function for graphing iterations vs all_testloss
-def graphLoss(iterations, all_testloss, title):
-    mp.figure()
-    mp.plot(iterations, all_testloss, label=title)
-    mp.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-              ncol=2, mode="expand", borderaxespad=0.)
-    mp.axhline(0, color='black')
-    mp.axvline(0, color='black')
-    mp.show()
-
 # Run Logistic regression with various iterations over the given datasets
-def runLogisticRegression(X_training, y_training, X_testing, y_testing):
-    iterations = [1, 3, 5, 10, 15]
-    all_testloss = []
-    for i in range(0, len(iterations)):
-        print "Starting logistic regression on data with %d iterations..." % iterations[i]
-        total_loss = 0
-        for j in range(0, 3):
-            trainingWeights = createWeights(X_training)
-            resultWeights = logisticRegressionSGD(X_training, y_training, trainingWeights, STEP_SIZE, LAMBDA,
-                                                  iterations[i])
-            test_loss = testingNewWeight(X_testing, y_testing, resultWeights)
-            total_loss += test_loss
-        all_testloss += [total_loss / 3]
+# TODO: Pick best num iterations
+def runLogisticRegression(X_training, y_training):
 
-    return iterations, all_testloss
+    trainingWeights = createWeights(X_training)
+    resultWeights = logisticRegressionSGD(X_training, y_training, trainingWeights, STEP_SIZE, LAMBDA, 5)
+
+    return resultWeights
 
 # Run K Nearest Neighbors on the dataset for various numbers of neighbors to fit to
-def runKNearestNeighbors(X_training, y_training, X_testing, y_testing):
+# TODO: Pick best num_neighbors
+def runKNearestNeighbors(X_training, y_training):
     print "Starting K Nearest Neighbors..."
     neighbors = [i * 10 for i in range(1, 60)]
-    KNN_all_testloss = []
     for neighbor in neighbors:
         clf = KNeighborsClassifier(neighbor, weights="uniform")
 
         clf.fit(X_training, y_training)
 
-        KNN_all_testloss += [clf.score(X_testing, y_testing)]
-    return neighbors, KNN_all_testloss
+    return clf
 
 # Run Random Forest on the dataset for various numbers of estimators
-def runRandomForest(X_training, y_training, X_testing, y_testing):
+# TODO: Pick best num_estimators
+def runRandomForest(X_training, y_training):
     print "Starting Random Forest..."
     num_estimators = [i for i in range(1, 40)]
-    RF_all_testloss = []
     for estimators in num_estimators:
         clf = RandomForestClassifier(max_depth=5, n_estimators=estimators, max_features='log2')
 
         clf.fit(X_training, y_training)
 
-        RF_all_testloss += [clf.score(X_testing, y_testing)]
-    return num_estimators, RF_all_testloss
+    return clf
 
 # Run a Neural Network on the dataset for various numbers of lambda values
-def runNeuralNetwork(X_training, y_training, X_testing, y_testing):
+# TODO: Pick best lambda value
+def runNeuralNetwork(X_training, y_training):
     print "Starting Neural Network..."
     lambda_values = [.000001, .0001, .1, 1, 2, 4, 8, 16, 32, 64, 128]
-    NN_all_testloss = []
     for lambdaVal in lambda_values:
         mlp = MLPClassifier(activation='logistic', solver='sgd', alpha=lambdaVal)
 
         mlp.fit(X_training, y_training)
 
-        NN_all_testloss += [mlp.score(X_testing, y_testing)]
-    return lambda_values, NN_all_testloss
+    return mlp
 
 # Feature selection on the given datasets
-def featureSelect(trainingDataset, testingDataset):
+def featureSelect(trainingDataset, testingDataset, bracketDataset):
     print "Starting Feature Selection..."
     X_training = trainingDataset[:, 3:]
     y_training = trainingDataset[:, 2]
@@ -148,48 +131,35 @@ def featureSelect(trainingDataset, testingDataset):
     X_testing = testingDataset[:, 3:]
     y_testing = testingDataset[:, 2]
 
+    bracket_testing = bracketDataset[:, 3:]
+
     randomlr = RandomizedLogisticRegression()
     X_training = randomlr.fit_transform(X_training, y_training)
     X_testing = randomlr.transform(X_testing)
+    bracket_testing = randomlr.transform(bracket_testing)
 
-    return X_training, y_training, X_testing, y_testing
+    return X_training, y_training, X_testing, y_testing, bracket_testing
 
 def main():
     # Pre-Process the data
-    trainingDataset, testingDataset = processData()
+    trainingDataset, testingDataset, bracketDataset = processData()
 
     # Feature Selection
-    X_training, y_training, X_testing, y_testing = featureSelect(trainingDataset, testingDataset)
+    X_training, y_training, X_testing, y_testing, bracket_testing = featureSelect(trainingDataset, testingDataset, bracketDataset)
 
     # Logistic Regression
-    (iterations, all_testloss) = runLogisticRegression(X_training, y_training, X_testing, y_testing)
+    resultWeights = runLogisticRegression(X_training, y_training)
 
     # KNN
-    (neighbors, KNN_all_testloss) = runKNearestNeighbors(X_training, y_training, X_testing, y_testing)
+    KNN_clf = runKNearestNeighbors(X_training, y_training)
 
     # Random Forest
-    (num_estimators, RF_all_testloss) = runRandomForest(X_training, y_training, X_testing, y_testing)
+    RF_clf = runRandomForest(X_training, y_training)
 
     # Neural Network
-    (lambda_values, NN_all_testloss) = runNeuralNetwork(X_training, y_training, X_testing, y_testing)
+    NN_mlp = runNeuralNetwork(X_training, y_training)
 
-    # Plot iteration vs. Percent right
-    print "Max percentage right with Logisitc Regression:\t %.10f" % max(all_testloss)
-    title = 'Logisitic Regression Correctly Predicted versus Iterations on Dataset'
-    graphLoss(iterations, all_testloss, title)
 
-    # Graph the number of neighbors to the percentage of correct guesses
-    print "Max percentage right with K Nearest Neighbors:\t %.10f" % max(KNN_all_testloss)
-    title = 'K Nearest Neighbors Correctly Predicted verses Num_Neighbors'
-    graphLoss(neighbors, KNN_all_testloss, title)
-
-    print "Max percentage right with Random Forest:\t\t %.10f" % max(RF_all_testloss)
-    title = 'Random Forest Correctly Predicted versus Num_estimators on Dataset'
-    graphLoss(num_estimators, RF_all_testloss, title)
-
-    print "Max percentage right with Neural Network:\t\t %.10f" % max(NN_all_testloss)
-    title = 'Neural Network Correctly Predicted versus Lambda Value on Dataset'
-    graphLoss(lambda_values, NN_all_testloss, title)
 
 
 def createWeights(dataset):
